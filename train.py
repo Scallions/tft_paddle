f2bc7ca77fd989eea6d34a314c9675bcafe91b44
yifwahaha <49463907+yifwahaha@users.noreply.github.com>
2021-08-12 09:32:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-# import lib
 from data.dataloader import load_data
 import pandas as pd
 import numpy as np
@@ -29,39 +28,39 @@ configs['batch_size'] = 64
 configs['encode_length'] = 168
 configs['attn_heads'] = 4
 configs['num_quantiles'] = 3
-configs['vailid_quantiles'] = [0.1,0.5,0.9]
+configs['vailid_quantiles'] = [0.1, 0.5, 0.9]
 configs['seq_length'] = 192
-configs['epochs'] = 10
-
+configs['epochs'] = 100
 
 ## train
 ### load model
-tft_model = model.create_model(configs) 
+tft_model = model.create_model(configs)
 ### load optimizer
 optimizer = utils.utils.create_optimizer(configs, tft_model)
 ### load dataloader
 train, val, test = data.load_data(configs)
 dataloader = data.create_dataloader(configs, train)
 ### loss func
-q_loss_func = model.QuantileLoss([0.1,0.5,0.9])
-
+q_loss_func = model.QuantileLoss([0.1, 0.5, 0.9])
+print(len(dataloader))
 ### train step
 #### epoch
 losses = []
-pbar = tqdm(range(configs["epochs"]))
-for epoch in pbar:
+#pbar = tqdm(range(configs["epochs"]))
+for epoch in range(configs["epochs"]):
     epoch_loss = [] # record loss
     ##### batch
-    for batch in dataloader:
+    for iter, batch in enumerate(dataloader):
         # output, encoder_ouput, decoder_output, attn, attn_weights = tft_model(batch)
         output, encoder_output, decoder_putput, attn, attn_weights, _, _ = tft_model(batch)
         loss = q_loss_func(output[:,:,:].reshape((-1,3)), batch['outputs'][:,:,0].flatten().astype('float32'))
         loss.backward()
         optimizer.step()
+        optimizer.clear_grad()
         epoch_loss.append(loss.item())
+        if (iter+1) % 5 == 0:
+            print(f"epoch:{epoch+1} \t iter: {iter+1} \t loss:{loss.item()}")
     losses.append(np.mean(epoch_loss))
-    pbar.set_description(f"epoch: {epoch} loss: {np.mean(epoch_loss)}")
 
-    
 
 ## output results
